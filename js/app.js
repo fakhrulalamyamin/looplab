c4c99044ab6d11f9adf18c2b4106f14bb0f454a9
fakhrulalamyamin <fakhrulalam.yamin143245@gmail.com>
2020-08-25 16:35:47 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -2,19 +2,23 @@ $("#year").text(new Date().getFullYear());
 
 $("body").scrollspy({ target: "#main-nav" });
 
-$('#main-nav a').on('click', function (e) {
-    // check if there hash
-    if (this.hash !== '') {
-      //prevent default
-      e.preventDefault();
-      // store hash
-      const hash = this.hash;
-      // adding animation 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 600, function () {
+$("#main-nav a").on("click", function (e) {
+  // check if there hash
+  if (this.hash !== "") {
+    //prevent default
+    e.preventDefault();
+    // store hash
+    const hash = this.hash;
+    // adding animation
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      600,
+      function () {
         // adding animation on window
         window.location.hash = hash;
-      })
-    }
-  })
+      }
+    );
+  }
+});
